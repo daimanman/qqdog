e29beb05eeafa34ecfc79fb035f01af5f94d98be
admin <2821224626@qq.com>
2018-11-26 21:47:08 +0800

    ok

---
+++ sql/qq.sql
@@ -0,0 +1,49 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : thinkpad
+Source Server Version : 50637
+Source Host           : 192.168.1.103:3306
+Source Database       : data_4
+
+Target Server Type    : MYSQL
+Target Server Version : 50637
+File Encoding         : 65001
+
+Date: 2018-11-26 21:46:18
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for qq
+-- ----------------------------
+DROP TABLE IF EXISTS `qq`;
+CREATE TABLE `qq` (
+  `uid` varchar(255) DEFAULT NULL,
+  `pwd` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of qq
+-- ----------------------------
+INSERT INTO `qq` VALUES ('168033989', '411');
+INSERT INTO `qq` VALUES ('1143886181', '411');
+INSERT INTO `qq` VALUES ('1748723478', '411');
+INSERT INTO `qq` VALUES ('1843594995', '26');
+INSERT INTO `qq` VALUES ('1934843846', '411');
+INSERT INTO `qq` VALUES ('2083499983', '411');
+INSERT INTO `qq` VALUES ('2109529257', '26');
+INSERT INTO `qq` VALUES ('2684839141', '2635');
+INSERT INTO `qq` VALUES ('2875593464', '411');
+INSERT INTO `qq` VALUES ('2972638700', '411');
+INSERT INTO `qq` VALUES ('3079693469', '411');
+INSERT INTO `qq` VALUES ('3120418574', '411');
+INSERT INTO `qq` VALUES ('3235875136', '411');
+INSERT INTO `qq` VALUES ('3356337217', '411');
+INSERT INTO `qq` VALUES ('3565759691', '411s');
+INSERT INTO `qq` VALUES ('1276742580', '987654321manxiao');
+INSERT INTO `qq` VALUES ('1332466059 ', '411');
+INSERT INTO `qq` VALUES ('2433276560 ', '411');
+INSERT INTO `qq` VALUES ('848443394 ', '411');
+INSERT INTO `qq` VALUES ('3422469468', '411');
